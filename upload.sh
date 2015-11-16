a1a77e7ea9931e2ee884ed09a8dc859b5ed2ce4b
Andreas Kloeckner <inform@tiker.net>
2015-11-16 00:02:49 -0600

    Add time planning spreadsheet

--- upload.sh
+++ upload.sh
@@ -4,12 +4,12 @@ set -e
 set -x
 
 TGT=tiker.net:public_html/tmp
-rsync -rav dist $TGT/sc15-tutorial-materials
+rsync -rav --delete dist $TGT/sc15-tutorial-materials
 rsync -rav sc15-pack.run $TGT
 rsync -rav sc15-tutorial-materials-dist.zip $TGT
 
 TGT=class@rl:dl/sc15-tutorial
-rsync -rav dist $TGT/materials
+rsync -rav --delete dist $TGT/materials
 rsync -rav sc15-pack.run $TGT
 rsync -rav sc15-tutorial-materials-dist.zip $TGT
 ssh class@rl "chmod a+rX dl -R"
