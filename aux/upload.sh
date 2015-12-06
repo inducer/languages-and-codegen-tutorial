1ea343b0d6ac31a89cee361de5c11ec6ce9d1b38
Andreas Kloeckner <inform@tiker.net>
2015-12-06 17:35:33 -0600

    Tweak logistics scripts

--- aux/upload.sh
+++ aux/upload.sh
@@ -3,15 +3,29 @@
 set -e
 set -x
 
-TGT=tiker.net:public_html/tmp
-rsync -rav --delete dist $TGT/sc15-tutorial-materials
-rsync -rav sc15-pack.run $TGT
-rsync -rav sc15-tutorial-materials-dist.zip $TGT
+TUT_ID=dsl
 
-TGT=class@rl:dl/sc15-tutorial
-rsync -rav --delete dist $TGT/materials
-rsync -rav sc15-pack.run $TGT
-rsync -rav sc15-tutorial-materials-dist.zip $TGT
+URL=http://andreask.cs.illinois.edu/tmp/$TUT_ID-tutorial-materials-dist.zip
+cp tut-pack.run tut-pack-custom.run
+sed -i s/MY_TUT_ID/$TUT_ID/ tut-pack-custom.run
+sed -i s/MYURL/$URL/ tut-pack-custom.run
+TGT=tiker.net:public_html/tmp
+
+rsync -rav --delete ../dist $TGT/$TUT_ID-tutorial-materials
+rsync -rav tut-pack-custom.run $TGT/$TUT_ID-tut-pack.run
+rsync -rav $TUT_ID-tutorial-materials-dist.zip $TGT
+
+URL=http://relate.cs.illinois.edu/dl/$TUT_ID/$TUT_ID-tutorial-materials-dist.zip
+cp tut-pack.run tut-pack-custom.run
+sed -i s/MY_TUT_ID/$TUT_ID/ tut-pack-custom.run
+sed -i s/MYURL/$URL/ tut-pack-custom.run
+TGT=class@rl:dl/$TUT_ID-tutorial
+
+rsync -rav --delete ../dist $TGT/materials
+rsync -rav tut-pack-custom.run $TGT/$TUT_ID-tut-pack.run
+rsync -rav ../$TUT_ID-tutorial-materials-dist.zip $TGT
 ssh class@rl "chmod a+rX dl -R"
+
+rm tut-pack-custom.run
 
 echo "COMPLETED"
